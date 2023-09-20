97acb686b2675e64b08614a0def4b5cf7b84ac58
Vaishnav Puthiyanamparambath <vaishnav.p@ikcontech.com>
2023-09-20 18:47:55 +0530

    initial commit

---
+++ src/main/java/com/ikn/ums/meetings/UmsMeetingsApplication.java
@@ -0,0 +1,13 @@
+package com.ikn.ums.meetings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmsMeetingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmsMeetingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ikn/ums/meetings/UmsMeetingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikn.ums.meetings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmsMeetingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
