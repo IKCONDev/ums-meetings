c2d43a01705082243957bef1b6682bc5f54e59ef
Bharat <Bharat@ikcontech.com>
2023-09-28 19:22:28 +0530

    Merge branch 'devbranch' of https://github.com/IKCONDev/ums-meetings.git into devbranch

--- src/main/java/com/ikn/ums/meeting/exception/ErrorCodeMessages.java
+++ src/main/java/com/ikn/ums/meeting/exception/ErrorCodeMessages.java
@@ -41,28 +41,28 @@ public class ErrorCodeMessages {
 	 public static final String ERR_MEETINGS_ACTIONITEMS_IDLIST_EMPTY_CODE = "ERR_MEETINGS_ACTIONITEMS_LIST_EMPTY_CODE-1013";
 	 public static final String ERR_MEETINGS_ACTIONITEMS_IDLIST_EMPTY_MESSAGE = "ActionItem id's list is empty or null";
 	 
-	 public static final String ERR_MEETINGS_ACTIONITEMS_GENERATE_CODE = "ERR_MEETINGS_ACTIONITEMS_GENERATE_CODE-1010";
+	 public static final String ERR_MEETINGS_ACTIONITEMS_GENERATE_CODE = "ERR_MEETINGS_ACTIONITEMS_GENERATE_CODE-1014";
 	 public static final String ERR_MEETINGS_ACTIONITEMS_GENERATE_MESSAGE = "Exception occured while generating action items";
 
-	 public static final String ERR_MEETINGS_TASKS_LIST_EMPTY_CODE ="ERR_MEETINGS_TASKS_LIST_EMPTY_CODE-1013";
+	 public static final String ERR_MEETINGS_TASKS_LIST_EMPTY_CODE ="ERR_MEETINGS_TASKS_LIST_EMPTY_CODE-1015";
 	 public static final String ERR_MEETINGS_TASKS_LIST_EMPTY_MEESAGE="Tasks list is empty";
 
-	 public static final String ERR_MEETINGS_TASKS_CONTROLLER_EXCEPTION_CODE = "ERR_TASKS_CONTROLLER_EXCEPTION_CODE-1014";
+	 public static final String ERR_MEETINGS_TASKS_CONTROLLER_EXCEPTION_CODE = "ERR_TASKS_CONTROLLER_EXCEPTION_CODE-1016";
 	 public static final String ERR_MEETINGS_TASKS_CONTROLLER_EXCEPTION_MESSAGE = "An Exception occured in Tasks Controller";
 	 
-	 public static final String ERR_MEETINGS_TASKS_SAVE_CODE="ERR_MEETINS_TASKS_SAVE_CODE-1015";
+	 public static final String ERR_MEETINGS_TASKS_SAVE_CODE="ERR_MEETINS_TASKS_SAVE_CODE-1017";
 	 public static final String ERR_MEETINS_TASKS_SAVE_MESSAGE="Exception occured while saving the task";
 	 
-	 public static final String ERR_MEETINGS_TASKS_UPDATE_CODE="ERR_MEETINS_TASKS_SAVE_CODE-1016";
+	 public static final String ERR_MEETINGS_TASKS_UPDATE_CODE="ERR_MEETINS_TASKS_SAVE_CODE-1018";
 	 public static final String ERR_MEETINGS_TASKS_UPDATE_MESSAGE="An Exception occured while updating the task";
 	
-	 public static final String ERR_MEETINGS_TASKS_CONVERTTASK_CODE="ERR_MEETINS_TASKS_SAVE_CODE-1017";
+	 public static final String ERR_MEETINGS_TASKS_CONVERTTASK_CODE="ERR_MEETINS_TASKS_SAVE_CODE-1019";
 	 public static final String ERR_MEETINGS_TASKS_CONVERTTASK_MESSAGE="An Exception occured while converting to task";
 	 
-	 public static final String ERR_MEETINGS_TASKS_ID_EMPTY_CODE = "ERR_MEETINS_TASKS_ID_EMPTY_CODE-1018";
+	 public static final String ERR_MEETINGS_TASKS_ID_EMPTY_CODE = "ERR_MEETINS_TASKS_ID_EMPTY_CODE-1020";
 	 public static final String ERR_MEETINGS_TASKS_ID_EMPTY_MEESAGE="Task id is empty";
 	 
-	 public static final String ERR_MEETINS_TASKS_DELETE_CODE = "ERR_MEETINS_TASKS_ID_EMPTY_CODE-1019";
+	 public static final String ERR_MEETINS_TASKS_DELETE_CODE = "ERR_MEETINS_TASKS_ID_EMPTY_CODE-1021";
 	 public static final String ERR_MEETINGS_TASKS_DELETE_MEESAGE="An Exception occured while deleting while deleting task";
 	 
 	 
