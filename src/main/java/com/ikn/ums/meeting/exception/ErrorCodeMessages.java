b8b00c8998f149b8d74ebd87bb014b9daf40f457
Vaishnav Puthiyanamparambath <vaishnav.p@ikcontech.com>
2023-09-28 12:20:03 +0530

    error code messages added

--- src/main/java/com/ikn/ums/meeting/exception/ErrorCodeMessages.java
+++ src/main/java/com/ikn/ums/meeting/exception/ErrorCodeMessages.java
@@ -2,7 +2,7 @@ package com.ikn.ums.meeting.exception;
 
 public class ErrorCodeMessages {
 	 
-	 public static final String ERR_MEETINGS_CONTROLLER_EXCEPTION_CODE = "EMEETINGS_CONTROLLER_EXCEPTION_CODE-1001";
+	 public static final String ERR_MEETINGS_CONTROLLER_EXCEPTION_CODE = "MEETINGS_CONTROLLER_EXCEPTION_CODE-1001";
 	 public static final String ERR_MEETINGS_CONTROLLER_EXCEPTION_MSG = "An Exception occured in Meetings controller";
 	 
 	 public static final String ERR_MEETINGS_USERID_EMPTY_EXCEPTION_CODE = "ERR_USER_EMPTY_EXCEPTION_CODE-1002";
@@ -10,5 +10,23 @@ public class ErrorCodeMessages {
 	 
 	 public static final String ERR_MEETINGS_LIST_EMPTY_CODE = "ERR_MEETINGS_LIST_EMPTY_CODE-1003";
 	 public static final String ERR_MEETINGS_LIST_EMPTY_MESSAGE = "Meetings list is empty";
+	 
+	 public static final String ERR_MEETINGS_ACTIONITEMS_ID_EMPTY_CODE = "ERR_MEETINGS_ID_EMPTY_CODE-1004";
+	 public static final String ERR_MEETINGS_ACTIONITEMS_ID_MESSAGE = "Action Item id is empty";
+	 
+	 public static final String ERR_MEETINGS_ACTIONITEMS_DELETE_CODE = "ERR_MEETINGS_ACTIONITEM_DELETE_CODE-1005";
+	 public static final String ERR_MEETINGS_ACTIONITEMS_DELETE_MESSAGE = "An exception occured while deleting action items";
+	 
+	 public static final String ERR_MEETINGS_ACTIONITEMS_LIST_EMPTY_CODE = "ERR_MEETINGS_ACTIONITEMS_LIST_EMPTY_CODE-1006";
+	 public static final String ERR_MEETINGS_ACTIONITEMS_LIST_EMPTY_MESSAGE = "ActionItems list is empty";
+	 
+	 public static final String ERR_MEETINGS_ACTIONITEMS_CONVERTTOTASK_CODE = "ERR_MEETINGS_ACTIONITEMS_CONVERTTOTASK_CODE-1007";
+	 public static final String ERR_MEETINGS_ACTIONITEMS_CONVERTTOTASK_MESSAGE = "Exception occured while converting action items to tasks";
+	 
+	 public static final String ERR_MEETINGS_ID_EMPTY_CODE = "ERR_MEETINGS_ID_EMPTY_CODE-1008";
+	 public static final String ERR_MEETINGS_ID_EMPTY_MESSAGE = "Meeting id is empty"; 
+	 
+	 public static final String ERR_MEETINGS_ACTIONITEMS_GET_CODE = "ERR_MEETINGS_ACTIONITEMS_GET_CODE-1009";
+	 public static final String ERR_MEETINGS_ACTIONITEMS_GET_MESSAGE = "Exception occured while getting action items";
 	 
 }
