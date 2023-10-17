0bcabe65631d6583ecda5b596fe2cbfebe556b21
Vaishnav Puthiyanamparambath <vaishnav.p@ikcontech.com>
2023-10-17 19:24:57 +0530

    Added create Meeting functionality

---
+++ src/main/java/com/ikn/ums/meeting/model/MeetingModel.java
@@ -0,0 +1,43 @@
+package com.ikn.ums.meeting.model;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MeetingModel {
+	
+		private String emailId;
+		private String subject;
+		private LocalDateTime startDateTime;
+		private LocalDateTime endDateTime;
+		private String location;
+	    private String[] attendees;
+	    private String organizerEmailId;
+	    private String organizerName;
+	    
+		private String insertedBy = "AUTO-BATCH-PROCESS";
+	    private String insertedDate = LocalDateTime.now().toString();
+	   
+		private String createdDateTime;
+		
+		private LocalDateTime modifiedDateTime;
+		
+		private String createdBy;
+		
+		private String modifiedBy;
+		
+		private String createdByEmailId;
+		
+		private String modifiedByEmailId;
+		
+		//other relations
+		//@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+		//@JoinColumn(name = "meeting_fk_id", nullable = false)
+		//private Set<ActionItem> actionItems;
+		
+}
